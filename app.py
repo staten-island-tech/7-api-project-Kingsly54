4a5312c9178143839f0a6374a653e1365f3c324d
Kingsly54 <kingslyw@nycstudents.net>
2026-05-04 09:21:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import requests
-
-def km():
+def play():
     response = requests.get("https://strangerthingsquotes.shadowdev.xyz/api/quotes")
     if response.status_code != 200:
         print("Error fetching data!")
@@ -8,15 +7,24 @@ def km():
     
     data = response.json()
     corrcounter=0
+    ccount=0
+    numplay=0
     for i in range(len(data)):
         answer=data[i]["author"]
         print(data[i]["quote"])
         ask=input("Who said this quote?")
         if ask==data[i]["author"]:
-            corrcounter+=1  
+            ccount+=1
+            corrcounter+=1
             print("You got it correct!")
             print(f"You have gotten {corrcounter} correct in a row!")
+            print(f"You have gotten {ccount}/{gg} correct!")
+            print(numplay)
         else:
             corrcounter=0
+            print(numplay)
             print(f"You got it wrong, the correct answer was {answer}")
-km()
+            print(f"You have gotten {ccount}/{gg} questions correct!")
+gg=int(input("How many times would you like to play?"))
+for i in range(gg):
+    play()
