44541bfed8cf3f38720b94d8763457905830af44
Kingsly54 <kingslyw@nycstudents.net>
2026-05-03 22:37:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,27 @@ def km():
         return None
     
     data = response.json()
-    for i in len(data):
-        print(data[i]["quote"])
+    corrcounter=0
+    wow=True
+    while wow==True:
+        for i in range(len(data)):
+            answer=data[i]["author"]
+            print(data[i]["quote"])
+            ask=input("Who said this quote?")
+            if ask==data[i]["author"]:
+                corrcounter+=1  
+                print("You got it correct!")
+                print(f"You have gotten {corrcounter} correct in a row!")
+                for i in range(len(data)):
+                    answer=data[i]["author"]
+                    print(data[i]["quote"])
+                    agsk=input("Would you like to try another one?").lower()
+                    if agsk=="no":
+                        wow=False
+            else:
+                corrcounter=0
+                print(f"You got it wrong, the correct answer was {answer}")
+                agsk=input("Would you like to try another one?").lower()
+                if agsk=="no":
+                    wow=False
 km()
